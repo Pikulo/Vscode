fcd9323d11fc2591d6bb03701d05dbea06859907
Pikulo <2950771552@qq.com>
2022-07-22 17:23:41 +0800

    add code

--- Projects/简单/1266. 访问所有点的最小时间.py
+++ Projects/简单/1266. 访问所有点的最小时间.py
@@ -8,4 +8,24 @@
 #     必须按照数组中出现的顺序来访问这些点。
 #     在访问某个点时，可以经过该点后面出现的点，但经过的那些点不算作有效访问。
 
+#思路：计算插差值
+points = [[559,511],[932,618],[-623,-443]]
+
+i=0
+sum=0
+while(i<len(points)):
+    if i+1<len(points):
+        list1=points[i]
+        list2=points[i+1]
+        print(list1,list2)
+        a=min(abs(list1[0]-list2[0]),abs(list1[1]-list2[1]))
+        #注意b这儿是绝对值差的绝对值
+        b=abs(abs(list1[0]-list2[0])-abs(list1[1]-list2[1]))
+        print(a,b)
+        sum+=a
+        sum+=b
+    i+=1
+print(sum)
+
+    
 
