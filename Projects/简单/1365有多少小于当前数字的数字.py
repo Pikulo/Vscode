133681e29329786efe71e0b07d9458362d33d58b
Pikulo <2950771552@qq.com>
2022-07-22 23:36:01 +0800

    fix

--- Projects/简单/1365有多少小于当前数字的数字.py
+++ Projects/简单/1365有多少小于当前数字的数字.py
@@ -1,6 +1,9 @@
-# nums=[8,1,2,2,3]
+from numpy import sort
+
+
+nums=[8,1,2,2,3]
 # nums = [6,5,4,8]
-nums = [7,7,7,7]
+# nums = [7,7,7,7]
 list_out=[]
 for i in nums:
     sum=0
@@ -10,3 +13,14 @@ for i in nums:
     list_out.append(sum)
 print(list_out)
 # return list_out
+#法2：利用哈希表
+hashmap={}
+arr=sorted(nums)
+print(arr)
+for j in range(len(arr)):
+    if arr[j] not in hashmap:
+        hashmap[arr[j]]=j
+print(hashmap)
+print(map(lambda x: hashmap[x],nums))
+#<map object at 0x0000022E0501BAF0>
+print(list(map(lambda x: hashmap[x],nums)))
