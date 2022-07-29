a04a0a6dcf0ca4c23e07b425055f94da94601295
Pikulo <2950771552@qq.com>
2022-07-29 11:15:23 +0800

    add

--- chap/栈/stack.py
+++ chap/栈/stack.py
@@ -12,8 +12,16 @@ class Stack:
     def push(self, item):
         self.items.append(item)
         # print("你给栈顶加了个%s" % item)
+    # 1. The pop method is defined in the Stack class.
+    # 2. The pop method is called on the self object.
+    # 3. The pop method returns the last item in the list.
+    # 4. The last item in the list is removed from the list.
+    # 5. The last item is returned to the calling method.
     def pop(self):
         return self.items.pop()
+    # 1. The peek method returns the last item in the list.
+    # 2. The peek method does not remove the last item.
+    # 3. The peek method can be called from outside the Stack class.
     def peek(self):
         return self.items[len(self.items) - 1]
     def size(self):
