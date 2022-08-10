39c9bc56172ca0ab784ba83aef11cdc31f6a4134
Pikulo <2950771552@qq.com>
2022-08-11 01:05:50 +0800

    add

--- Projects/LEETCODE/简单/431. 拥有最多糖果的孩子.py
+++ Projects/LEETCODE/简单/431. 拥有最多糖果的孩子.py
@@ -1,3 +1,10 @@
+# Given a list of candies, check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+# 
+# Args:
+#   candies: an array of integers representing the number of candies that each child has.
+#   extraCandies: the number of candies that the person brings to the class
+# Returns:
+#   A list of boolean values.
 candies = [12,12]
 
 extraCandies = 1
