0a7584bd6d38e034a4cd2c61b6780aab829678f6
Pikulo <2950771552@qq.com>
2022-08-25 22:44:06 +0800

    add code

---
+++ Projects/LEETCODE/简单/2315_统计星号.py
@@ -0,0 +1,28 @@
+# s = "l|*e*et|c**o|*de|"
+# s = "iamprogrammer"
+s = "yo|uar|e**|b|e***au|tifu|l"
+s = "|**|*|*|**||***||"
+list_tem = []
+list_tem2 = []
+for i,j in enumerate(s):
+    if j == '|':
+        list_tem.append(i)
+# print(list_tem)
+if len(list_tem)>0:
+    i =0
+    while i <= len(list_tem)-2:
+        # print(i)
+        # print(s[list_tem[i]:list_tem[i+1]+1])
+        list_tem2.append(s[list_tem[i]:list_tem[i+1]+1])
+        i +=2
+    print(list_tem2)
+    for i in list_tem2:
+        s = s.replace(i, '',1)
+        print(s)
+res = 0
+for i in s:
+    if i == '*':
+        res+=1
+print(res)
+# return res
+
