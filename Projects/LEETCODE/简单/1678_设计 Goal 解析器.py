5c445a1a8b908a0a91b64befaa44af517f90ab68
Pikulo <2950771552@qq.com>
2022-08-29 10:33:10 +0800

    add code

--- Projects/LEETCODE/简单/1678_设计 Goal 解析器.py
+++ Projects/LEETCODE/简单/1678_设计 Goal 解析器.py
@@ -1,1 +1,11 @@
 command = "G()(al)"
+
+# my idea
+command = command.replace('()', 'o')
+command = command.replace('(al)', 'al')
+print(command)
+# return command
+
+# other idea
+print(command.replace('()', 'o').replace('(al)', 'al'))
+
