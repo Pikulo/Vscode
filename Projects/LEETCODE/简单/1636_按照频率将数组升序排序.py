c2a2c2c719aac4772d6225ed1a8496a7f26c6f59
Pikulo <2950771552@qq.com>
2022-09-19 19:00:06 +0800

    add

--- Projects/LEETCODE/简单/1636_按照频率将数组升序排序.py
+++ Projects/LEETCODE/简单/1636_按照频率将数组升序排序.py
@@ -2,4 +2,17 @@
 # 请你将数组按照每个值的频率 升序 排序。如果有多个值的频率相同，
 # 请你按照数值本身将它们 降序 排序。 
 # 请你返回排序后的数组。
+from audioop import mul
+from traceback import print_tb
+from typing import Counter
 
+
+nums = [1,1,2,2,2,3]
+nums = Counter(nums)
+nums=sorted(nums.items(),key=lambda x:x[1])
+# print(nums)
+# for i in nums:
+    # print(i[0],i[1])
+dic = {}
+for i in nums:
+    
